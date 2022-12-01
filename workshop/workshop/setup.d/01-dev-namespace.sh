013665365b0659623a2064eeeb70419aa8958bc5
Julien Kolaric <jkolaric@jkolaric56Y96.vmware.com>
2022-12-01 18:30:05 +0100

    Add postgres-password

--- workshop/workshop/setup.d/01-dev-namespace.sh
+++ workshop/workshop/setup.d/01-dev-namespace.sh
@@ -96,7 +96,7 @@ spec:
 EOF
 
 
-kubectl create secret generic db-binding-compatible --from-literal=type=postgresql --from-literal=provider=vac --from-literal=jdbc-url=jdbc:postgresql://emoji-postgresql.${SESSION_NAMESPACE}.svc.cluster.local:5432/emoji --from-literal=password=emoji --from-literal=username=postgres
+kubectl create secret generic db-binding-compatible --from-literal=type=postgresql --from-literal=provider=vac --from-literal=jdbc-url=jdbc:postgresql://emoji-postgresql.${SESSION_NAMESPACE}.svc.cluster.local:5432/emoji --from-literal=password=emoji --from-literal=username=postgres --from-literal=postgres-password=emoji
 
 tanzu service claim create db-binding-compatible \
   --resource-name db-binding-compatible \
