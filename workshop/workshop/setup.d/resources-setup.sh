017c2c0824ea1f82434cc100c9e9bef02ae44dc2
Timo Salm <tsalm@vmware.com>
2023-05-09 16:45:54 +0200

    Initial commit

--- workshop/workshop/setup.d/resources-setup.sh
+++ workshop/workshop/setup.d/resources-setup.sh
@@ -9,7 +9,4 @@ kubectl apply -f ~/samples/scan-policy.yaml
 
 kubectl create secret generic db-binding-compatible --from-literal=type=postgresql --from-literal=provider=vac --from-literal=jdbc-url=jdbc:postgresql://emoji-postgresql.${SESSION_NAMESPACE}.svc.cluster.local:5432/emoji --from-literal=password=emoji --from-literal=username=postgres --from-literal=postgres-password=emoji
 
-tanzu service claim create db-binding-compatible \
-  --resource-name db-binding-compatible \
-  --resource-kind Secret \
-  --resource-api-version v1
+tanzu services resource-claim create db-binding-compatible --resource-name db-binding-compatible --resource-kind Secret --resource-api-version v1
